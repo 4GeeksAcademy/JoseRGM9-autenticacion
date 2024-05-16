2f858af724c64ffeeb11037896008511c835691b
JoseRGM9 <157386833+JoseRGM9@users.noreply.github.com>
2024-05-16 19:10:40 +0000

    commitfin

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,13 +1,35 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { Context } from "../store/appContext";
 
 export const Navbar = () => {
+	const { store, actions } = useContext(Context);
+	const [isLogged, setIsLogged] = useState(false)
+	const token = localStorage.getItem("token");
+
+	useEffect(() => {
+		if (store.isLogged == false) {
+			setIsLogged(false)
+		} else if (store.isLogged == true) {
+			setIsLogged(true)
+		}
+		console.log(isLogged)
+	}, [store.isLogged])
+	const handleLogout = () => {
+		const token = localStorage.removeItem("token");
+		actions.logOut();
+	}
 	return (
 		<nav className="navbar navbar-light bg-light">
 			<div className="container">
 				<Link to="/">
 					<span className="navbar-brand mb-0 h1">Autenticacion</span>
 				</Link>
+				<div className="ml-auto">
+					{isLogged ? <Link to="/">
+						<span className="btn btn-danger navbar-brand mb-0 h1" onClick={() => handleLogout()}>Logout</span>
+					</Link> : ""}
+				</div>
 			</div>
 		</nav>
 	);
